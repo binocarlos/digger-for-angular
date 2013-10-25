baabad871f09bb51a732193bef7c7c945fa7eca6
Kai Davenport <kaiyadavenport@gmail.com>
2013-10-25 22:25:55 +0000

    XML processing

--- index.js
+++ index.js
@@ -35,3 +35,120 @@ angular
     'digger.filters',
     'digger.repeat'
   ])
+  .config(['$provide', function ($provide) {
+    
+    $provide.factory('xmlHttpInterceptor', ['xmlFilter', function (xmlFilter) {
+      return function (promise) {
+        return promise.then(function (response) {
+          response.xml = xmlFilter(response.data);
+          return response;
+        });
+      };
+    }]);
+    
+  }])
+  .factory('xmlEncoder', function(){
+
+    function string_factory(data, depth){
+
+      var meta = data._digger || {};
+      var children = data._children || [];
+      var attr = data;
+      depth = depth || 0;
+
+      function get_indent_string(){
+        var st = "\t";
+        var ret = '';
+        for(var i=0; i<depth; i++){
+          ret += st;
+        }
+        return ret;
+      }
+
+      var pairs = {};
+
+      if(meta.id && meta.id.length>0){
+        pairs.id = meta.id;
+      }
+
+      if(meta.class && angular.isArray(meta.class) && meta.class.length>0){
+        pairs.class = meta.class.join(' ');
+      }
+
+      var pair_strings = [];
+
+      Object.keys(attr || {}).forEach(function(key){
+        var val = attr[key];
+        if(key.indexOf('_')===0){
+          return;
+        }
+        if(typeof(val)=='string'){
+          val = '' + val;
+        }
+        if(val && val.length>0){
+          pairs[key] = val;  
+        }
+      })
+      
+      Object.keys(pairs || {}).forEach(function(field){
+        var value = pairs[field];
+      
+        if(value!=null && value!=undefined){
+          pair_strings.push(field + '="' + value + '"');  
+        }
+      })
+
+      if(children && children.length>0){
+        var ret = get_indent_string() + '<' + meta.tag + ' ' + pair_strings.join(' ') + '>' + "\n";
+
+        children.forEach(function(child){      
+          ret += string_factory(child, depth+1);
+        })
+
+        ret += get_indent_string() + '</' + meta.tag + '>' + "\n";
+
+        return ret;    
+      }
+      else{
+        return get_indent_string() + '<' + meta.tag + ' ' + pair_strings.join(' ') + ' />' + "\n";
+      }
+    }
+
+    return string_factory;
+
+  })
+
+  .factory('xmlParser', ['$window', function ($window) {
+
+    function MicrosoftXMLDOMParser() {
+      this.parser = new $window.ActiveXObject('Microsoft.XMLDOM');
+    }
+
+    MicrosoftXMLDOMParser.prototype.parse = function (input) {
+      this.parser.async = false;
+      return this.parser.loadXml(input);
+    };
+
+    function XMLDOMParser() {
+      this.parser = new $window.DOMParser();
+    }
+
+    XMLDOMParser.prototype.parse = function (input) {
+      return this.parser.parseFromString(input, 'text/xml');
+    };
+
+    if ($window.DOMParser) {
+      return new XMLDOMParser();
+    } else if ($window.ActiveXObject) {
+      return new MicrosoftXMLDOMParser();
+    } else {
+      throw new Error('Cannot parser XML in this environment.');
+    }
+
+  }])
+  .filter('xml', ['xmlParser', function (xmlParser) {
+    return function (input) {
+      var xmlDoc = xmlParser.parse(input);
+      return angular.element(xmlDoc);
+    };
+  }])
